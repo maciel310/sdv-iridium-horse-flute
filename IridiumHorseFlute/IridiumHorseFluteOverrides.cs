ecab2d8e4d1cb3a9b71f19c1a2c71f73ef532beb
maciel310 <maciel310@gmail.com>
2024-04-20 16:37:00 -0600

    Port over logic from BetterHorseFlute implementation with changes for the 1.6 API updates. Rename to 'Iridium Hose Flute', add add to Clint's shop as an upgrade from the regular Horse Flute

---
+++ IridiumHorseFlute/IridiumHorseFluteOverrides.cs
@@ -0,0 +1,49 @@
+﻿using StardewValley;
+using SObject = StardewValley.Object;
+using StardewValley.Characters;
+
+namespace Iridium_Horse_Flute
+{
+    internal class IridiumHorseFluteOverrides
+    {
+        public static bool beginUsing(Tool __instance, GameLocation location, int x, int y, Farmer who, ref bool __result)
+        {
+            if (!IsIridiumHorseFlute(__instance)) return true;
+
+            // Only process action if this is for the current player.
+            if (who == null || !Game1.player.Equals(who)) return false;
+
+            Game1.toolAnimationDone(who);
+            who.CanMove = true;
+            who.canReleaseTool = false;
+            who.UsingTool = false;
+
+            var flute = ItemRegistry.Create<SObject>("(O)911");
+            flute.performUseAction(location);
+
+            var horse = Utility.findHorseForPlayer(who.UniqueMultiplayerID);
+            if (WillWarpHorse(horse, who))
+            {
+                DelayedAction.functionAfterDelay(() => horse.checkAction(who, location), 1700);
+            }
+
+            __result = false;
+
+            return false;
+        }
+
+        private static bool WillWarpHorse(Horse horse, Farmer who)
+        {
+            // These are the same checks that the standard Horse Flute performs before warping the horse.
+            return horse != null
+                && (Math.Abs(who.Tile.X - horse.Tile.X) > 1
+                    || Math.Abs(who.Tile.Y - horse.Tile.Y) > 1)
+                && Utility.GetHorseWarpRestrictionsForFarmer(who) == Utility.HorseWarpRestrictions.None;
+        }
+
+        private static bool IsIridiumHorseFlute(Item tool)
+        {
+            return tool.QualifiedItemId == "(T)AnthonyMaciel.IridiumHorseFlute_Flute";
+        }
+    }
+}
---
+++ IridiumHorseFlute/ModEntry.cs
@@ -0,0 +1,46 @@
+﻿using StardewModdingAPI;
+using StardewValley.GameData.Tools;
+using StardewValley;
+using HarmonyLib;
+using Iridium_Horse_Flute;
+
+namespace IridiumHorseFlute
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            ToolData data = new()
+            {
+                ClassName = "GenericTool",
+                Name = "IridiumHorseFlute",
+                DisplayName = "Iridium Horse Flute",
+                Description = "An upgraded Horse Flute that automatically mounts the horse when summoned.",
+                SetProperties = new Dictionary<string, string> { { "InstantUse", "true" } },
+                UpgradeFrom = new List<ToolUpgradeData> { 
+                    new()
+                    {
+                        Price = 1000,
+                        RequireToolId = "(O)911",
+                        TradeItemId = "(O)337",
+                        TradeItemAmount = 2,
+                    }
+                },
+            };
+            DataLoader.Tools(Game1.content).Add("AnthonyMaciel.IridiumHorseFlute_Flute", data);
+
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Tool), nameof(Tool.beginUsing)),
+                prefix: new HarmonyMethod(typeof(IridiumHorseFluteOverrides), nameof(IridiumHorseFluteOverrides.beginUsing))
+            );
+            
+        }
+    }
+}
